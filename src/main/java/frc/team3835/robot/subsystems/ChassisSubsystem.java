9d393b402a5d806a4ba229fad28323b01a74425f
TomerSivan <98271399+TomerSivan@users.noreply.github.com>
2023-07-27 16:32:11 +0300

    Itai CR Fixes

--- src/main/java/frc/team3835/robot/subsystems/ChassisSubsystem.java
+++ src/main/java/frc/team3835/robot/subsystems/ChassisSubsystem.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team3835.robot.Constants;
 import frc.team3835.robot.Constants.SwerveConstants;
 
-public class ChassisSubsystem extends SubsystemBase { // CR: document
+public class ChassisSubsystem extends SubsystemBase { 
   // An enum with the names of the wheel modules
   public enum wheels {
     left_front, right_front, right_back, left_back
@@ -25,22 +25,42 @@ public class ChassisSubsystem extends SubsystemBase { // CR: document
   // Gyro 
   private AHRS imu;
 
-  public ChassisSubsystem() { // CR: don't pass the white line
-    this.swerve_modules[wheels.left_front.ordinal()] = new SwerveModule(Constants.ChassisConstants.LEFT_FRONT_DRIVE,
+  public ChassisSubsystem() { 
+    this.swerve_modules[wheels.left_front.ordinal()] = new SwerveModule(Constants.ChassisConstants.LEFT_FRONT_DRIVE, // Instancing Left Front Wheel
             Constants.ChassisConstants.LEFT_FRONT_STEER,
             Constants.ChassisConstants.LEFT_FRONT_ENC,
             Constants.ChassisConstants.LEFT_FRONT_ZERO,
             Constants.ChassisConstants.LEFT_FRONT_INVERTED);
-    this.swerve_modules[wheels.right_front.ordinal()] = new SwerveModule(Constants.ChassisConstants.RIGHT_FRONT_DRIVE, Constants.ChassisConstants.RIGHT_FRONT_STEER, Constants.ChassisConstants.RIGHT_FRONT_ENC, Constants.ChassisConstants.RIGHT_FRONT_ZERO, Constants.ChassisConstants.RIGHT_FRONT_INVERTED);
-    this.swerve_modules[wheels.left_back.ordinal()] = new SwerveModule(Constants.ChassisConstants.LEFT_BACK_DRIVE, Constants.ChassisConstants.LEFT_BACK_STEER, Constants.ChassisConstants.LEFT_BACK_ENC, Constants.ChassisConstants.LEFT_BACK_ZERO, Constants.ChassisConstants.LEFT_BACK_INVERTED);
-    this.swerve_modules[wheels.right_back.ordinal()] = new SwerveModule(Constants.ChassisConstants.RIGHT_BACK_DRIVE, Constants.ChassisConstants.RIGHT_BACK_STEER, Constants.ChassisConstants.RIGHT_BACK_ENC, Constants.ChassisConstants.RIGHT_BACK_ZERO, Constants.ChassisConstants.RIGHT_BACK_INVERTED);
+
+    this.swerve_modules[wheels.right_front.ordinal()] = new SwerveModule(Constants.ChassisConstants.RIGHT_FRONT_DRIVE, // Instancing Right Front Wheel
+            Constants.ChassisConstants.RIGHT_FRONT_STEER, 
+            Constants.ChassisConstants.RIGHT_FRONT_ENC, 
+            Constants.ChassisConstants.RIGHT_FRONT_ZERO, 
+            Constants.ChassisConstants.RIGHT_FRONT_INVERTED);
+
+    this.swerve_modules[wheels.left_back.ordinal()] = new SwerveModule(Constants.ChassisConstants.LEFT_BACK_DRIVE, // Instancing Left Back Wheel
+    Constants.ChassisConstants.LEFT_BACK_STEER, 
+    Constants.ChassisConstants.LEFT_BACK_ENC, 
+    Constants.ChassisConstants.LEFT_BACK_ZERO, 
+    Constants.ChassisConstants.LEFT_BACK_INVERTED);
+
+    this.swerve_modules[wheels.right_back.ordinal()] = new SwerveModule(Constants.ChassisConstants.RIGHT_BACK_DRIVE, // Instancing Right Back Wheel
+    Constants.ChassisConstants.RIGHT_BACK_STEER, 
+    Constants.ChassisConstants.RIGHT_BACK_ENC, 
+    Constants.ChassisConstants.RIGHT_BACK_ZERO, 
+    Constants.ChassisConstants.RIGHT_BACK_INVERTED);
 
     setDefaultCommand(null); // TODO: Default Command
   }
 
-  // Gets a chassisSpeeds and boolean to set the module states
+ /**
+     * Sets the module's state to given one
+     *
+     * @param chassisSpeeds The desired chassisSpeeds object for module velocities
+     * @param fieldRelative  Is field relative or not
+     */
   public void drive(ChassisSpeeds chassisSpeeds, boolean fieldRelative) {
-    // makes a swerve module-state array from chassisSpeeds
+    // Makes a swerve module-state array from chassisSpeeds
     var swerveModuleStates = Constants.ChassisConstants.kDriveKinematics.toSwerveModuleStates(fieldRelative
       ?ChassisSpeeds.fromFieldRelativeSpeeds(chassisSpeeds, this.imu.getRotation2d().unaryMinus())
       :chassisSpeeds
@@ -48,7 +68,14 @@ public class ChassisSubsystem extends SubsystemBase { // CR: document
     setModuleStates(swerveModuleStates);
   }
 
-  // Gets an xVelocity a yVelocity a rotation value and a boolean to set the module states
+  /**
+     * Sets the module's state to given one
+     *
+     * @param xVelocity  The velocity on the x axis
+     * @param yVelocity  The velocity on the y axis
+     * @param rot  The rotational velocity
+     * @param fieldRelative  Is field relative or not
+     */
   public void drive(double xVelocity, double yVelocity, double rot, boolean fieldRelative) {
     // Kinematics turns the Chassis speeds to desired swerveModule states depending on if field relative or not
     var swerveModuleStates =
@@ -59,18 +86,27 @@ public class ChassisSubsystem extends SubsystemBase { // CR: document
     setModuleStates(swerveModuleStates);
   }
   
-  // Gets an array of 4 module states to set as the desired states
+
+  /**
+     * Sets the desired states of the modules to given ones
+     *
+     * @param desiredStates  Desired array of 4 module states
+     */
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     // Sets max acceleration and velocity to wheels
     SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.ChassisConstants.kMaxSpeedMetersPerSecond);
     
+    // Uses the set method of the SwerveModule to declare desired state of the module
     this.swerve_modules[wheels.left_front.ordinal()].set(desiredStates[0]);
     this.swerve_modules[wheels.right_front.ordinal()].set(desiredStates[1]);
     this.swerve_modules[wheels.left_back.ordinal()].set(desiredStates[2]);
     this.swerve_modules[wheels.right_back.ordinal()].set(desiredStates[3]);
   }
 
-  // Stop all motors of the modules
+  /**
+     * Stops all modules
+     *
+     */
   public void stopModules() {
       this.swerve_modules[wheels.left_front.ordinal()].stopMotors();
       this.swerve_modules[wheels.right_front.ordinal()].stopMotors();
