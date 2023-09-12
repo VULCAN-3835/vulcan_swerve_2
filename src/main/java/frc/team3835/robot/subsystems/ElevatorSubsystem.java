f1ddfafe5e0ab9f1940d09364df93d6ec6b3d41f
hadaslevin <98271399+TomerSivan@users.noreply.github.com>
2023-09-12 19:04:06 +0300

    Added Elevator Control

--- src/main/java/frc/team3835/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/team3835/robot/subsystems/ElevatorSubsystem.java
@@ -5,17 +5,54 @@
 package frc.team3835.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team3835.robot.Constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
   private TalonFX elevatorMotor;
+
+  private DigitalInput limitSwitch;
+
+  private final int PID_SLOT = 0;
+  private final int TIMEOUT_MS = 30;
+
+  private final double maxVelocity = 0; // TODO: Find max velocity
+  private final double maxAcceleration = 0; // Todo: Find max Acceleration
+
+  
   public ElevatorSubsystem() {
     elevatorMotor = new TalonFX(ElevatorConstants.ELEVATOR_MOTOR);
+
     elevatorMotor.configFactoryDefault();
-    elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, PID_SLOT, TIMEOUT_MS);
+
+    elevatorMotor.configMotionCruiseVelocity((int) maxVelocity);
+    elevatorMotor.configMotionAcceleration((int) maxAcceleration);
+
+    elevatorMotor.config_kP(PID_SLOT, 0); // TODO: Find pid values
+    elevatorMotor.config_kI(PID_SLOT, 0);
+    elevatorMotor.config_kD(PID_SLOT, 0);
+    elevatorMotor.config_kF(PID_SLOT, 0);
+  }
+
+  public void setArmPosition(double position) {
+    this.elevatorMotor.set(TalonFXControlMode.MotionMagic, position);
+  }
+  public void setPower(double power) {
+    this.elevatorMotor.set(TalonFXControlMode.PercentOutput, power);
+  }
+  public void zeroMotors() {
+    this.elevatorMotor.set(TalonFXControlMode.PercentOutput, 0);
+  }
+  public double getCurrentPosition () {
+    return this.elevatorMotor.getSelectedSensorPosition();
+  }
+  public double getCurrentTargetPosition() {
+    return this.elevatorMotor.getClosedLoopTarget();
   }
 
   @Override
